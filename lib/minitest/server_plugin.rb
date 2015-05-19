bee222a54ef1318f8efdd57ca4ecf86524232b19
Ryan Davis <ryand@zenspider.com>
2015-05-18 18:02:13 -0800

    + Sanitize failures before serializing if UnexpectedErrors have bad data. [git-p4: depot-paths = "//src/minitest-server/dev/": change = 10248]

--- lib/minitest/server_plugin.rb
+++ lib/minitest/server_plugin.rb
@@ -32,7 +32,29 @@ class Minitest::ServerReporter < Minitest::AbstractReporter
     r = result
     c = r.class
     file, = c.instance_method(r.name).source_location
+    sanitize r.failures
+
     @mt_server.result file, c.name, r.name, r.failures, r.assertions, r.time
+  end
+
+  def sanitize failures
+    failures.map! { |e|
+      case e
+      when Minitest::UnexpectedError then
+        # embedded exception might not be able to be marshaled.
+        bt = e.exception.backtrace
+
+        ex = e.exception = RuntimeError.new e.message
+        ex.set_backtrace bt
+
+        if ex.instance_variables.include? :@bindings then # web-console is Evil
+          ex.instance_variable_set :@bindings, nil
+          e.instance_variable_set  :@bindings, nil
+        end
+      end
+
+      e
+    }
   end
 
   def report
